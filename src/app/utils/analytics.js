b2f321751e7a07de8bc0353187db320c5cef8102
lucas-bresson <contact.bresson@gmail.com>
2024-11-02 12:40:08 +0100

    rm value from sendGAEvent

--- src/app/utils/analytics.js
+++ src/app/utils/analytics.js
@@ -3,7 +3,6 @@ export const sendGAEvent = ({ action, category, label }) => {
       window.gtag('event', action, {
         event_category: category,
         event_label: label,
-        value: value,
       });
     }
   };
