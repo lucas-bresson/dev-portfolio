9d4ce8df84e0b360cc6c2a117f1d926b3391dd47
lucas-bresson <contact.bresson@gmail.com>
2024-11-04 10:49:12 +0100

    -

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=G-DJT3ZZKR3Y`}
         strategy="afterInteractive"
       />
       <Script
@@ -36,7 +36,8 @@ export default function RootLayout({
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
-      gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+
+      gtag('config', 'G-DJT3ZZKR3Y');
     `,
         }}
       />
